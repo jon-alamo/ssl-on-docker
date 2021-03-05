1c6a38fa2ebce6b8bacbf0e4bf8b9f91eae75faf
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-03-05 17:50:18 +0100

    bugfix -> test-https/init-nginx-test.sh wrong config dir

--- data/test-https/init-nginx-test.sh
+++ data/test-https/init-nginx-test.sh
@@ -2,7 +2,7 @@
 
 # Replace envs in nginx config template
 export DOLLAR='$'
-envsubst < /repo/nginx.conf.template > /etc/nginx/conf.d/app.conf
+envsubst < /repo/data/test-https/nginx.conf.https.test.template > /etc/nginx/conf.d/app.conf
 
 #Run nginx
 nginx -g "daemon off;"
