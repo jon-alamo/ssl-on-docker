13eedf5f6cf4f017817de053f78582c754dfe55a
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-03-05 17:43:08 +0100

    bugfix -> test-https/test.yml replace command by entrypoint

--- data/test-https/init-nginx-test.sh
+++ data/test-https/init-nginx-test.sh
@@ -2,7 +2,7 @@
 
 # Replace envs in nginx config template
 export DOLLAR='$'
-envsubst < ../../nginx.conf.template > /etc/nginx/app.conf
+envsubst < /repo/nginx.conf.template > /etc/nginx/app.conf
 
 #Run nginx
 nginx -g "daemon off;"
