8d2dca2ad97b85221b449c9fd8cc4a28f81af6b5
joni <jonatan@jacoti.com>
2022-12-21 17:22:19 +0100

    Added  placeholders to be used with envsubst

--- run-nginx.prod.sh
+++ run-nginx.prod.sh
@@ -10,7 +10,7 @@ fi
 
 # Replace envs in nginx config template
 export DOLLAR='$'
-envsubst < /nginx-conf/app.conf > /etc/nginx/conf.d/app.conf
+envsubst '${HOST_NAME},${DOLLAR}' < /nginx-conf/app.conf > /etc/nginx/conf.d/app.conf
 
 #Run nginx
 nginx -g "daemon off;"
